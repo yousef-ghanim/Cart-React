13586b2bc60aa27e7bcd68948d40726896baf033
yousef-ghanim <yousefghanim85@gmail.com>
2021-04-11 18:56:50 +0000

    Updates

---
+++ precache-manifest.27ef0c8a8a7e0d1a84c20912d9bf2b13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "657d5759b38bd6fefde00774f60cf460",
+    "url": "/cart-react/index.html"
+  },
+  {
+    "revision": "77e4821b6e81e728eb6a",
+    "url": "/cart-react/static/css/main.35134b96.chunk.css"
+  },
+  {
+    "revision": "2829f658d05344f4543e",
+    "url": "/cart-react/static/js/2.4904b4fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cart-react/static/js/2.4904b4fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77e4821b6e81e728eb6a",
+    "url": "/cart-react/static/js/main.5f625e48.chunk.js"
+  },
+  {
+    "revision": "33e71f27b6ac1b32ae61",
+    "url": "/cart-react/static/js/runtime-main.df0cd489.js"
+  }
+]);
